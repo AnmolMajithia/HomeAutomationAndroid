a57e2615cb99ddf8427555906fe8f4855a2a3f10
AnmolMajithia <rush2asm@gmail.com>
2020-06-16 15:40:23 +0530

    First Working Build Complete and Tested

--- app/src/main/java/com/anmol/roomcontrol/MainActivity.java
+++ app/src/main/java/com/anmol/roomcontrol/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
@@ -15,6 +16,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton settingsButton;
@@ -32,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
         ml = false;
         sl = false;
@@ -82,10 +87,8 @@ public class MainActivity extends AppCompatActivity {
 
     protected void connectToNodeMCU(int val) {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String ip = sharedPreferences.getString("ipAddr", "");
-        int port = Integer.parseInt(sharedPreferences.getString("port", "69"));
-        Log.d("gateeeem", "yay - " + ip);
-        Log.d("phoey", "yay - " + port);
+        String ip = sharedPreferences.getString("ipAddr", "192.168.0.169");
+        int port = Integer.parseInt(Objects.requireNonNull(sharedPreferences.getString("port", "69")));
         try {
             Socket socket = new Socket(ip, port);
             OutputStream outStream = socket.getOutputStream();
@@ -113,16 +116,16 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    protected void secondaryLightToggle() {
-        sl = !sl;
-        if (sl) {
-            secondaryLight.setBackgroundColor(Color.parseColor("#009688")); //Pastel green
-            secondaryLight.setImageResource(R.drawable.ic_bulbon);
+    protected void fanToggle() {
+        f = !f;
+        if (f) {
+            fan.setBackgroundColor(Color.parseColor("#009688")); //Pastel green
+            fan.setImageResource(R.drawable.ic_fanon);
             connectToNodeMCU(3);
         }
         else {
-            secondaryLight.setBackgroundColor(Color.parseColor("#F44336")); // Pastel red
-            secondaryLight.setImageResource(R.drawable.ic_bulboff);
+            fan.setBackgroundColor(Color.parseColor("#F44336")); // Pastel red
+            fan.setImageResource(R.drawable.ic_fanoff);
             connectToNodeMCU(4);
         }
     }
@@ -141,16 +144,16 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    protected void fanToggle() {
-        f = !f;
-        if (f) {
-            fan.setBackgroundColor(Color.parseColor("#009688")); //Pastel green
-            fan.setImageResource(R.drawable.ic_fanon);
+    protected void secondaryLightToggle() {
+        sl = !sl;
+        if (sl) {
+            secondaryLight.setBackgroundColor(Color.parseColor("#009688")); //Pastel green
+            secondaryLight.setImageResource(R.drawable.ic_bulbon);
             connectToNodeMCU(7);
         }
         else {
-            fan.setBackgroundColor(Color.parseColor("#F44336")); // Pastel red
-            fan.setImageResource(R.drawable.ic_fanoff);
+            secondaryLight.setBackgroundColor(Color.parseColor("#F44336")); // Pastel red
+            secondaryLight.setImageResource(R.drawable.ic_bulboff);
             connectToNodeMCU(8);
         }
     }
